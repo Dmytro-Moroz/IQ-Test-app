a1d0eaed4bdf21460b0ada44d4f74244f69077a3
DmytroMoroz <avangardi@ukr.net>
2020-06-24 13:05:53 +0300

    first commit

---
+++ IQTestApp/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace IQTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.button1.Location = new System.Drawing.Point(12, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Повернутися";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(790, 436);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Наступна";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(203, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(581, 468);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(203, 546);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(313, 546);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 21);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(427, 546);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(37, 21);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(529, 546);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(37, 21);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(634, 546);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(37, 21);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(742, 546);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(37, 21);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.8F);
+            this.label1.Location = new System.Drawing.Point(435, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Питання № 1";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.9F);
+            this.button3.Location = new System.Drawing.Point(790, 561);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 53);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Закінчити тестування";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(43)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(950, 626);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IQ Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ IQTestApp/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IQTestApp
+{
+    public partial class Form1 : Form
+    {
+        private int picNum = 0;
+        private int[] answers;
+        public Form1()
+        {
+            InitializeComponent();
+            answers = new int[7];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var image1 = new Bitmap(IQTestApp.Properties.Resources.t1);
+            pictureBox1.BackgroundImage = image1;
+        }
+
+        public void ShowPic(int current)
+        {
+            int picNum = current + 1;
+            label1.Text = "Питання # " + picNum;
+
+            switch (answers[current])
+            {
+                case 0:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    radioButton5.Checked = false;
+                    radioButton6.Checked = false;
+                    break;
+                case 1:
+                    radioButton1.Checked = true;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    radioButton5.Checked = false;
+                    radioButton6.Checked = false;
+                    break;
+                case 2:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = true;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    radioButton5.Checked = false;
+                    radioButton6.Checked = false;
+                    break;
+                case 3:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = true;
+                    radioButton4.Checked = false;
+                    radioButton5.Checked = false;
+                    radioButton6.Checked = false;
+                    break;
+                case 4:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = true;
+                    radioButton5.Checked = false;
+                    radioButton6.Checked = false;
+                    break;
+                case 5:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    radioButton5.Checked = true;
+                    radioButton6.Checked = false;
+                    break;
+                case 6:
+                    radioButton1.Checked = false;
+                    radioButton2.Checked = false;
+                    radioButton3.Checked = false;
+                    radioButton4.Checked = false;
+                    radioButton5.Checked = false;
+                    radioButton6.Checked = true;
+                    break;
+            }
+
+            switch (current)
+            {
+                case 0:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t1);
+
+                    break;
+                case 1:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t2);
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t3);
+                    break;
+                case 3:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t4);
+                    break;
+                case 4:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t5);
+                    break;
+                case 5:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t8);
+                    break;
+                case 6:
+                    pictureBox1.BackgroundImage = new Bitmap(IQTestApp.Properties.Resources.t7);
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            picNum++;
+
+            if (picNum > 6)
+            {
+                picNum = 6;
+            }
+            
+            ShowPic(picNum);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picNum--;
+
+            if (picNum < 0)
+            {
+                picNum = 0;
+            }
+
+            ShowPic(picNum);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            answers[picNum] = 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            answers[picNum] = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            answers[picNum] = 3;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            answers[picNum] = 4;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            answers[picNum] = 5;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            answers[picNum] = 6;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int correct = 0;
+            
+            if (answers[0] == 4)
+            {
+                correct++;
+            }
+            if (answers[1] == 5)
+            {
+                correct++;
+            }
+            if (answers[2] == 1)
+            {
+                correct++;
+            }
+            if (answers[3] == 2)
+            {
+                correct++;
+            }
+            if (answers[4] == 3)
+            {
+                correct++;
+            }
+            if (answers[5] == 2)
+            {
+                correct++;
+            }
+            if (answers[6] == 2)
+            {
+                correct++;
+            }
+
+            int correctPercent = correct *100 /7;
+
+            if (correctPercent >= 30 && correctPercent <= 60)
+            {
+                MessageBox.Show("Слабенько! твій відсоток точних відповідей дорівнює: " + correctPercent + " %");
+            }
+            if (correctPercent >= 60)
+            {
+                MessageBox.Show("Непогано! твій відсоток правильних відповідей дорівнює: " + correctPercent + " %");
+            }
+            else if(correctPercent < 30)
+            {
+                MessageBox.Show("Дуже слабенько, трэба підвчитися! твій відсоток правильних відповідей дорівнює: " + correctPercent + " %");
+            }
+        }
+    }
+}
